c6610cfa76e3d5e97664dcfae0f42d2d6064da73
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-10-14 19:48:20 -0400

    imported Home.js into App.js

--- intro/src/components/App.js
+++ intro/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Home from './Home'
 
 class App extends Component {
   render() {
     return (
       <div>
         Hello World
+        <Home />
       </div>
     )
   }
