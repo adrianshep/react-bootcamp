bc8edfe990d23f1b549909a2388ddfbd3d17dbd7
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-10-20 15:13:25 -0400

    swapping in match.url and .path

--- intro/src/components/Topics.js
+++ intro/src/components/Topics.js
@@ -14,19 +14,19 @@ export default function Topics() {
       <h2>Topics</h2>
         <ul>
           <li>
-            <Link to='/topics/rendering' >Rendering with React</Link>
+            <Link to={`${match.url}/rendering`}>Rendering with React</Link>
           </li>
           <li>
-            <Link to='/topics/components' >Components</Link>
+            <Link to={`${match.url}/components`} >Components</Link>
           </li>
           <li>
-            <Link to='/topics/props-v-state' >Props vs State</Link>
+            <Link to={`${match.url}/props-v-state`}>Props vs State</Link>
           </li>
         </ul>
 
         <hr />
 
-        <Route path='/topics/:topicId' component={Topic} />
+        <Route path={`${match.path}/:topicId`} component={Topic} />
     </div>
   )
 }
